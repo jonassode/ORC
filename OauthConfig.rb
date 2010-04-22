77a65d23103b17f58b58452199790a6c6d2b6c77
Jonas Soderstrom <jonas.soderstrom@gmail.com>
2010-04-22 14:25:13 +0200

    updated values

--- OauthConfig.rb
+++ OauthConfig.rb
@@ -1,11 +1,4 @@
 module OauthConfig
-#   CONSUMER = "consumer1"
-#   SECRET = "consumerSecret12"
-   CONSUMER = "3ec57af6-c4d6-4d3c-9bdc-14fafa633b18"
-   SECRET = "ooAxC8UhcT4COmsxmcNbmtt7eDvtiC"
-#   CONSUMER = "tcflashclient"
-#   SECRET = "flashClientSecret"
-   SITE = "http://login.test.projektzion.se:81"
-   CALLBACK = "http://www.google.com"
-#   USERID = "F4C5ED2CD3D44F958A7C4E40045150C7"
+   CONSUMER = "consumer"
+   SECRET = "consumerSecret"
 end
